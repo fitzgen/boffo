e4dd23b6094c29abb5e9884146326d64c89f2f06
Matt Forbes <matt@freshplum.com>
2012-06-28 22:17:48 -0700

    6 basic user API calls implemented

--- boffo/src/boffo_api_user.erl
+++ boffo/src/boffo_api_user.erl
@@ -1,1 +1,30 @@
 -module(boffo_api_user).
+
+-export([set_online/2, get_online/1, add_game/2, remove_game/2, get_games/1, get_users/1]).
+
+%% Tokens
+
+set_online(Username, Value) ->
+    Server = pg2:get_closest_pid(boffo_user_status_server),
+    gen_server:call(Server, {set_online, Username, Value}).
+
+get_online(Username) ->
+    Server = pg2:get_closest_pid(boffo_user_status_server),
+    gen_server:call(Server, {get_online, Username}).
+
+add_game(Username, Game_Id) ->
+    Server = pg2:get_closest_pid(boffo_user_status_server),
+    gen_server:call(Server, {add_game, Username, Game_Id}).
+
+remove_game(Username, Game_Id) ->
+    Server = pg2:get_closest_pid(boffo_user_status_server),
+    gen_server:call(Server, {remove_game, Username, Game_Id}).
+
+get_games(Username) ->
+    Server = pg2:get_closest_pid(boffo_user_status_server),
+    gen_server:call(Server, {get_games, Username}).
+
+get_users(Game_Id) ->
+    Server = pg2:get_closest_pid(boffo_user_status_server),
+    gen_server:call(Server, {get_users, Game_Id}).
+
--- boffo_user/src/boffo_user_app.erl
+++ boffo_user/src/boffo_user_app.erl
@@ -13,7 +13,23 @@ start() ->
     start(normal, []).
 
 start(_StartType, _StartArgs) ->
+    ensure_schema(),
+    mnesia:start(),
+
+    pg2:create(boffo_user_status_server),
+    
     boffo_user_sup:start_link().
 
 stop(_State) ->
     ok.
+
+ensure_schema() ->
+    Node = node(),
+    case mnesia:create_schema([Node]) of
+	ok ->
+	    ok;
+	{error, {Node, {already_exists, Node}}} ->
+	    ok;
+	{error, Error} ->
+	    {error, Error}
+    end.
